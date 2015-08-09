fd3fc6ce6d5f1098dab5b8b3f525a4358279b62b
Marwan Aziz <aziz.marwan@gmail.com>
2015-08-09 15:24:19 -0400

    fix object sync with firebase

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
 angular.module('crowdfund', ['firebase'])
-    .controller('AppController', ['$scope', 'FirebaseRef', function ($scope, FirebaseRef) {
-        FirebaseRef('campaignName').$bindTo($scope, 'campaignName');
-        $scope.description = 'Be a part of helping hundreds of millions of children to read, write, and perform arithmetic.';
+    .controller('AppController', ['$scope', 'campaign', '$firebaseObject', function ($scope, campaign, $firebaseObject) {
+        $scope.campaignName = $firebaseObject(campaign.child('name'));
+        $scope.tagline = $firebaseObject(campaign.child('tagline'));
+        $scope.description = $firebaseObject(campaign.child('description'));
     }])
-    .factory('FirebaseRef',['$firebaseObject', function ($firebaseObject) {
-        return function (key) {
-            var ref = new Firebase("https://incandescent-fire-2805.firebaseio.com/"+key);
-            return $firebaseObject(ref);
-        }
+    .factory('campaign',['$firebaseObject', function ($firebaseObject) {
+        var ref = new Firebase("https://incandescent-fire-2805.firebaseio.com");
+        return ref.child('campaign');
     }])
-    .controller('StatusController', ['$scope', '$firebaseObject', 'FirebaseRef', function ($scope, $firebaseObject, FirebaseRef) {
-        FirebaseRef('amountRaised').$bindTo($scope, "amountRaised");
-        FirebaseRef('numPeople').$bindTo($scope, "numPeople");
+    .controller('StatusController', ['$scope', '$firebaseObject', 'campaign', function ($scope, $firebaseObject, campaign) {
 
+        $scope.amountRaised = $firebaseObject(campaign.child('amountRaised'));
+        $scope.numPeople = $firebaseObject(campaign.child('numPeople'));
     }])
-    .controller('ContributeController', ['$scope', '$firebaseObject', 'FirebaseRef', function ($scope, $firebaseObject, FirebaseRef) {
-        FirebaseRef('amountRaised').$bindTo($scope, "amountRaised");
-        FirebaseRef('numPeople').$bindTo($scope, "numPeople");
-
+    .controller('ContributeController', ['$scope', '$firebaseObject', 'campaign', function ($scope, $firebaseObject, campaign) {
+        $scope.amountRaised = 0;
+        $scope.numPeople = 0;
         $scope.contribs = [
             100,
             400,
             1000,
             2500
         ];
+        $firebaseObject(campaign.child('amountRaised')).$bindTo($scope, 'amountRaised');
+        $firebaseObject(campaign.child('numPeople')).$bindTo($scope, 'numPeople');
+
         $scope.contribute = function (amount) {
-            console.dir(FirebaseRef);
-            var ref = FirebaseRef('amountRaised');
-            ref.$loaded().then(function(data){
-                data += amount;
-                ref.$save();
-            });
-            console.log('You have contributed ' + amount);
+            console.log(amount);
+            $scope.amountRaised.$value += amount;
+            $scope.numPeople.$value += 1;
+            // $firebaseObject(campaign.child('amountRaised')).$loaded().then(function(data){
+            //     data.$value += amount;
+            //     data.$save();
+            // });
+            // $firebaseObject(campaign.child('numPeople')).$loaded().then(function(data){
+            //     data.$value += 1;
+            //     data.$save();
+            // });
         };
 
     }])
